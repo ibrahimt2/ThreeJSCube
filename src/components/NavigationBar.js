05b17b5331d638f21c6f5e4f5681eefa192eb9bf
ibrahimt2 <zerobottles@outlook.com>
2023-01-28 10:34:58 -0500

    set navbar class name

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function ColorSchemesExample() {
+function NavigationBar() {
     return (
         <>
             <Navbar bg="dark" variant="dark">
