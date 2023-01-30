4df49a9dba50e5dbfe0836693a72a941277e0cec
ibrahimt2 <zerobottles@outlook.com>
2023-01-30 13:08:26 -0500

    Create columns for X, Y, Z inputs

--- src/components/CommandAdd/CommandAdd.js
+++ src/components/CommandAdd/CommandAdd.js
@@ -7,8 +7,32 @@ function CommandAdd({cubeMachineState, send}) {
     return(
         <Form>
             <Row>
-                <Col xs={4} sm={4} md={4} lg={4} xl={4} xxl={4}>
+                <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
                     <Form.Label>Name</Form.Label>
+                    <Form.Control placeholder="Name"
+                    value={cubeMachineState.context.formName}
+                    onChange={(e) => {
+                        send("Form input changed", {value: e.target.value});
+                    }}></Form.Control>
+                </Col>
+                <Col xs={2} sm={2} md={2} lg={2} xl={2} xxl={2}>
+                    <Form.Label>X</Form.Label>
+                    <Form.Control placeholder="Name"
+                    value={cubeMachineState.context.formName}
+                    onChange={(e) => {
+                        send("Form input changed", {value: e.target.value});
+                    }}></Form.Control>
+                </Col>
+                <Col xs={2} sm={2} md={2} lg={2} xl={2} xxl={2}>
+                    <Form.Label>Y</Form.Label>
+                    <Form.Control placeholder="Name"
+                    value={cubeMachineState.context.formName}
+                    onChange={(e) => {
+                        send("Form input changed", {value: e.target.value});
+                    }}></Form.Control>
+                </Col>
+                <Col xs={2} sm={2} md={2} lg={2} xl={2} xxl={2}>
+                    <Form.Label>Z</Form.Label>
                     <Form.Control placeholder="Name"
                     value={cubeMachineState.context.formName}
                     onChange={(e) => {
