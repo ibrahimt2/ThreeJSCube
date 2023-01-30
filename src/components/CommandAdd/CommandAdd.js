3c22f127ddfb6a2e9b924cce9ecfaf0555427b78
ibrahimt2 <zerobottles@outlook.com>
2023-01-30 14:40:05 -0500

    Add dropdown options for commands

--- src/components/CommandAdd/CommandAdd.js
+++ src/components/CommandAdd/CommandAdd.js
@@ -2,19 +2,26 @@ import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import { Typeahead } from 'react-bootstrap-typeahead';
 
 function CommandAdd({cubeMachineState, send}) {
+
+    let availableCommands = ['MOVE TO', 'MOVE BY', 'ROTATE TO', 'ROTATE BY']
 
     return(
         <Form>
             <Row>
                 <Col xs={6} sm={6} md={6} lg={6} xl={6} xxl={6}>
                     <Form.Label>Name</Form.Label>
-                    <Form.Control placeholder="Name"
-                    value={cubeMachineState.context.formName}
+                    <Typeahead
+                    id="basic-typeahead-single"
                     onChange={(e) => {
-                        send("Form input changed", {value: e.target.value, valueType: 'name'});
-                    }}></Form.Control>
+                        send("Form input changed", {value: e, valueType: 'name'});
+                    }}
+                    options={availableCommands}
+                    placeholder='Select Command'
+                    selected={cubeMachineState.context.formName}>
+                    </Typeahead>
                 </Col>
                 <Col xs={2} sm={2} md={2} lg={2} xl={2} xxl={2}>
                     <Form.Label>X</Form.Label>
