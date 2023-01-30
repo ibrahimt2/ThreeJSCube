7de2242bb67f351885ddc9fba0771d99a175ea97
ibrahimt2 <zerobottles@outlook.com>
2023-01-30 15:31:05 -0500

    write logic for command processing

--- src/utils/commandProcessing.js
+++ src/utils/commandProcessing.js
@@ -1,11 +1,22 @@
 function computeCommandListResult(position, rotation, commandArray) {
     
+
     positionResult = position
     rotationResult = rotation
 
     while(commandArray.length != 0) {
         command = commandArray.shift()
 
-        if(command.name == 'MOVE TO')
+        if(command.name == 'MOVE TO') {
+            positionResult = [command.x, command.y, command.z]
+        } else if(command.name == 'ROTATE TO') {
+            rotationResult = [command.x, command.y, command.z]
+        } else if(command.name == 'MOVE BY') {
+            positionResult = [positionResult[0] + command.x, positionResult[1] + command.y, positionResult[2] + command.z]
+        } else if(command.name == 'ROTATE BY') {
+            rotationResult = [rotationResult[0] + command.x, rotationResult[1] + command.y, rotationResult[2] + command.z]
+        }
     }
+
+    return [positionResult, rotationResult]
 }
