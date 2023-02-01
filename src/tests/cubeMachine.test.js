96aaa4644d7b843652d84e0e07c2690e180ba85d
ibrahimt2 <zerobottles@outlook.com>
2023-02-01 13:42:53 -0500

    write tests for MOVE TO, MOVE BY and ROTATE TO, ROTATE BY commands

--- src/tests/cubeMachine.test.js
+++ src/tests/cubeMachine.test.js
@@ -1,5 +1,6 @@
 import cubeMachine from '../machines/cubeMachine.js'
 import { interpret, withContext } from 'xstate';
+import { degToRad } from '../utils/constant.js';
 
 describe('cubeMachine', () => {
     let machine;
@@ -20,81 +21,45 @@ describe('cubeMachine', () => {
         service = interpret(machine).start();
     });
 
-    it('should start in the BuildCommandList state', () => {
+    it('starts in the BuildCommandList state', () => {
         expect(service.state.value).toBe('BuildCommandList');
     });
 
-    it('should update formName context on "Form input changed" event with valueType name', () => {
+    it('updates formName context on "Form input changed" event with valueType name', () => {
 
         service.send("Form input changed", { value: 'MOVE TO', valueType: 'name' })
 
-        expect(service.state.context).toEqual({
-            position: [0, 0, 0],
-            rotation: [0, 0, 0],
-            commands: [],
-            error: null,
-            formName: 'MOVE TO',
-            formX: '',
-            formY: '',
-            formZ: '',
-        });
+        expect(service.state.context.formName).toEqual('MOVE TO');
     });
 
-    it('should update x context on "Form input changed" event with valueType x', () => {
+    it('updates x context on "Form input changed" event with valueType x', () => {
 
         service.send("Form input changed", { value: '10', valueType: 'x' })
 
-        expect(service.state.context).toEqual({
-            position: [0, 0, 0],
-            rotation: [0, 0, 0],
-            commands: [],
-            error: null,
-            formName: '',
-            formX: '10',
-            formY: '',
-            formZ: '',
-        });
+        expect(service.state.context.formX).toEqual('10');
     });
 
-    it('should update y context on "Form input changed" event with valueType y', () => {
+    it('updates y context on "Form input changed" event with valueType y', () => {
 
         service.send("Form input changed", { value: '10', valueType: 'y' })
 
-        expect(service.state.context).toEqual({
-            position: [0, 0, 0],
-            rotation: [0, 0, 0],
-            commands: [],
-            error: null,
-            formName: '',
-            formX: '',
-            formY: '10',
-            formZ: '',
-        });
+        expect(service.state.context.formY).toEqual('10');
     });
 
-    it('should update z context on "Form input changed" event with valueType z', () => {
+    it('updates z context on "Form input changed" event with valueType z', () => {
 
         service.send("Form input changed", { value: '10', valueType: 'z' })
 
-        expect(service.state.context).toEqual({
-            position: [0, 0, 0],
-            rotation: [0, 0, 0],
-            commands: [],
-            error: null,
-            formName: '',
-            formX: '',
-            formY: '',
-            formZ: '10',
-        });
+        expect(service.state.context.formZ).toEqual('10');
     });
 
-    it('should transition to Command Processing state on "Process list commands" event', () => {
+    it('transitions to Command Processing state on "Process list commands" event', () => {
         service.send('Process list commands');
 
         expect(service.state.value).toBe('Command Processing');
     });
 
-    it('should add command to command list on "Add Command" event with valid input', () => {
+    it('adds command to command list on "Add Command" event with valid input', () => {
 
         service.send("Form input changed", { value: 'MOVE TO', valueType: 'name' })
         service.send("Form input changed", { value: '10', valueType: 'x' })
@@ -102,42 +67,103 @@ describe('cubeMachine', () => {
         service.send("Form input changed", { value: '30', valueType: 'z' })
         service.send('Add Command');
 
-        expect(service.state.context).toEqual({
-            position: [0, 0, 0],
-            rotation: [0, 0, 0],
-            commands: [{ name: 'MOVE TO', x: 10, y: 20, z: 30 }],
-            error: null,
-            formName: 'MOVE TO',
-            formX: '10',
-            formY: '20',
-            formZ: '30',
-        });
+        expect(service.state.context.commands).toEqual([{ name: 'MOVE TO', x: 10, y: 20, z: 30 }],
+);
     });
 
-    it('should correctly set the cube position using the MOVE TO command', async() => {
+    it('sets the cube position using the MOVE TO command', async() => {
 
         service.send("Form input changed", { value: 'MOVE TO', valueType: 'name'})
         service.send("Form input changed", { value: '10', valueType: 'x' })
         service.send("Form input changed", { value: '20', valueType: 'y' })
         service.send("Form input changed", { value: '30', valueType: 'z' })
         service.send('Add Command');
-        // await waitUntilState(service, state => state.value == 'BuildCommandList')
         await service.send('Process list commands');
-        
-        // await waitUntilState(service, state => state.value == 'Command Processing')
-        // await waitUntilState(service, state => state.value == 'BuildCommandList')
+    
+        expect(service.state.context.position).toEqual([10, 20, 30]);
+    });
 
+    it('aggregates MOVE BY commands to set the cube position', async() => {
 
-        
-        expect(service.state.context).toEqual({
-            position: [10, 20, 30],
-            rotation: [0, 0, 0],
-            commands: [],
-            error: null,
-            formName: 'MOVE TO',
-            formX: '10',
-            formY: '20',
-            formZ: '30',
-        });
+        service.send("Form input changed", { value: 'MOVE BY', valueType: 'name'})
+        service.send("Form input changed", { value: '1', valueType: 'x' })
+        service.send("Form input changed", { value: '1', valueType: 'y' })
+        service.send("Form input changed", { value: '1', valueType: 'z' })
+        service.send('Add Command');
+
+        service.send("Form input changed", { value: 'MOVE BY', valueType: 'name'})
+        service.send("Form input changed", { value: '2', valueType: 'x' })
+        service.send("Form input changed", { value: '2', valueType: 'y' })
+        service.send("Form input changed", { value: '2', valueType: 'z' })
+        service.send('Add Command');
+        await service.send('Process list commands');
+    
+        expect(service.state.context.position).toEqual([3, 3, 3]);
+    });
+
+    it('set the cubes roation using the ROTATE TO command', async() => {
+
+        service.send("Form input changed", { value: 'ROTATE TO', valueType: 'name'})
+        service.send("Form input changed", { value: '50', valueType: 'x' })
+        service.send("Form input changed", { value: '60', valueType: 'y' })
+        service.send("Form input changed", { value: '70', valueType: 'z' })
+        service.send('Add Command');
+        await service.send('Process list commands');
+    
+        expect(service.state.context.rotation).toEqual([50, 60, 70]);
+    });
+
+    it('aggregates ROTATE BY commands to set the cube position', async() => {
+
+        service.send("Form input changed", { value: 'ROTATE BY', valueType: 'name'})
+        service.send("Form input changed", { value: '30', valueType: 'x' })
+        service.send("Form input changed", { value: '20', valueType: 'y' })
+        service.send("Form input changed", { value: '10', valueType: 'z' })
+        service.send('Add Command');
+
+        service.send("Form input changed", { value: 'ROTATE BY', valueType: 'name'})
+        service.send("Form input changed", { value: '10', valueType: 'x' })
+        service.send("Form input changed", { value: '20', valueType: 'y' })
+        service.send("Form input changed", { value: '30', valueType: 'z' })
+        service.send('Add Command');
+        await service.send('Process list commands');
+    
+        expect(service.state.context.rotation).toEqual([40, 40, 40]);
+    });
+
+    it('ROTATE TO command overwrites previous changes to cube rotation', async() => {
+
+        service.send("Form input changed", { value: 'ROTATE BY', valueType: 'name'})
+        service.send("Form input changed", { value: '30', valueType: 'x' })
+        service.send("Form input changed", { value: '20', valueType: 'y' })
+        service.send("Form input changed", { value: '10', valueType: 'z' })
+        service.send('Add Command');
+
+        service.send("Form input changed", { value: 'ROTATE TO', valueType: 'name'})
+        service.send("Form input changed", { value: '100', valueType: 'x' })
+        service.send("Form input changed", { value: '100', valueType: 'y' })
+        service.send("Form input changed", { value: '100', valueType: 'z' })
+        service.send('Add Command');
+        await service.send('Process list commands');
+    
+        expect(service.state.context.rotation).toEqual([100, 100, 100]);
+    });
+
+    it('MOVE TO command overwrites previous changes to cube position', async() => {
+
+        service.send("Form input changed", { value: 'MOVE BY', valueType: 'name'})
+        service.send("Form input changed", { value: '50', valueType: 'x' })
+        service.send("Form input changed", { value: '20', valueType: 'y' })
+        service.send("Form input changed", { value: '10', valueType: 'z' })
+        service.send('Add Command');
+
+        service.send("Form input changed", { value: 'ROTATE TO', valueType: 'name'})
+        service.send("Form input changed", { value: '1', valueType: 'x' })
+        service.send("Form input changed", { value: '1', valueType: 'y' })
+        service.send("Form input changed", { value: '1', valueType: 'z' })
+        service.send('Add Command');
+        await service.send('Process list commands');
+    
+        expect(service.state.context.rotation).toEqual([1, 1, 1]);
     });
 })
